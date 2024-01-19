890fa515897a66a9409d6c2221085fefe11fcd9f
Matheus Marinho <matheus.marinhodsp@gmail.com>
2024-01-19 15:05:48 -0300

    feat: finaliza rotas para criar, editar, e excluir tasks

--- app/backend/src/routes/tasks.routes.ts
+++ app/backend/src/routes/tasks.routes.ts
@@ -5,5 +5,8 @@ const tasksController = new TasksController();
 const tasksRouter = Router();
 
 tasksRouter.get('/tasks/:userId', tasksController.findAllUserTasks);
+tasksRouter.post('/tasks/create', tasksController.create);
+tasksRouter.put('/tasks/update/:taskId', tasksController.updateTask);
+tasksRouter.delete('/tasks/delete/:taskId', tasksController.delete);
 
 export default tasksRouter;
--- app/backend/src/services/TasksServices.ts
+++ app/backend/src/services/TasksServices.ts
@@ -4,8 +4,66 @@ import TasksModel from '../models/TasksModel';
 export default class TasksServices {
   constructor(private tasksModel: TasksModel = new TasksModel()) {}
 
-  public async findAllUserTasks(userId: number): Promise<ITask[] | null> {
-    const result = this.tasksModel.findAllUserTasks(userId);
+  public async create(newTask: ITask): Promise<boolean> {
+    const result = await this.tasksModel.create(newTask);
+    return result;
+  }
+
+  public async findAllUserTasks(userId: string): Promise<ITask[] | null> {
+    const result = this.tasksModel.findAllUserTasks(Number(userId));
+    return result;
+  }
+
+  public async updateTitle(newTitle: string, taskId: string): Promise<string> {
+    const result = await this.tasksModel.updateTitle(newTitle, Number(taskId));
+    return result;
+  }
+
+  public async updateDescription(
+    newDescription: string,
+    taskId: string
+  ): Promise<string> {
+    const result = await this.tasksModel.updateDescription(
+      newDescription,
+      Number(taskId)
+    );
+    return result;
+  }
+
+  public async updateColor(newColor: string, taskId: string): Promise<string> {
+    const result = await this.tasksModel.updateColor(newColor, Number(taskId));
+    return result;
+  }
+
+  public async setFavorite(setFav: string, taskId: string): Promise<string> {
+    const result = await this.tasksModel.setFavorite(
+      setFav === 'true',
+      Number(taskId)
+    );
+    return result;
+  }
+
+  public async updateTask(
+    newValue: string,
+    keyToUpdate: string,
+    taskId: string
+  ): Promise<string> {
+    switch (keyToUpdate) {
+      case 'title':
+        return this.updateTitle(newValue, taskId);
+      case 'description':
+        return this.updateDescription(newValue, taskId);
+      case 'color':
+        return this.updateColor(newValue, taskId);
+      case 'favorite':
+        return this.setFavorite(newValue, taskId);
+      default:
+        throw new Error();
+    }
+  }
+
+  public async delete(taskId: string): Promise<string> {
+    const result = await this.tasksModel.delete(Number(taskId));
     return result;
   }
 }
