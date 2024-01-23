12870e4b44397d36a2268d5100f955f5d808e8a4
Matheus Marinho <matheus.marinhodsp@gmail.com>
2024-01-23 15:54:50 -0300

    feat: implementa logica incial e requests de login e registro

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { userRequest } from '../services/requests';
 import '../styles/Pages/login.sass';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogged, setLogged] = useState(false);
+  const [wrongPassword, setWrongPass] = useState(false);
+
+  const apiBaseUrl = process.env.REACT_APP_BASE_API_URL;
+
+  const registerNewUser = async () => {
+    try {
+      console.log(process.env);
+      const result = await userRequest(`${apiBaseUrl}users/register`, {
+        username,
+        name,
+        password,
+      });
+      localStorage.setItem('userInfos', { id: result.id, name: result.name });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const login = async () => {
+    try {
+      const result = await userRequest(`${apiBaseUrl}/users/login`, {
+        username,
+        password,
+      });
+      localStorage.setItem('userInfos', { id: result.id, name: result.name });
+      setLogged(true);
+    } catch (error) {
+      setWrongPass(true);
+    }
+  };
+
+  if (isLogged) return <Navigate to="/tasks" />;
   return (
     <div className="main">
       <input type="checkbox" id="chk" aria-hidden="true" />
@@ -14,11 +51,26 @@ function Login() {
             type="text"
             name="username"
             placeholder="Usuário"
-            required=""
+            value={username}
+            onChange={({ target: { value } }) => setUsername(value)}
           />
-          <input type="text" name="name" placeholder="Nome" required="" />
-          <input type="password" name="pswd" placeholder="Senha" required="" />
-          <button>Registrar</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            value={name}
+            onChange={({ target: { value } }) => setName(value)}
+          />
+          <input
+            type="password"
+            name="pswd"
+            placeholder="Senha"
+            value={password}
+            onChange={({ target: { value } }) => setPassword(value)}
+          />
+          <button onClick={registerNewUser} type="button">
+            Registrar
+          </button>
         </form>
       </div>
       <div className="login">
@@ -30,10 +82,20 @@ function Login() {
             type="text"
             name="username"
             placeholder="Usuário"
-            required=""
+            value={username}
+            onChange={({ target: { value } }) => setUsername(value)}
           />
-          <input type="password" name="pswd" placeholder="Senha" required="" />
-          <button>Login</button>
+          <input
+            type="password"
+            name="pswd"
+            placeholder="Senha"
+            value={password}
+            onChange={({ target: { value } }) => setPassword(value)}
+          />
+          {wrongPassword ? <p>Senha incorreta</p> : null}
+          <button onClick={login} type="button">
+            Login
+          </button>
         </form>
       </div>
     </div>
